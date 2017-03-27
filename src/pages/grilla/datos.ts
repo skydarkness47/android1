7ae7d56222177238933d0322dacefbe632d48da9
Jonathan Aguirre <yonathanaguirre@live.com>
2017-03-27 10:35:48 -0300

    primer commit

---
+++ src/pages/grilla/datos.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'grilla.html'
+})
+export class Grilla {
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+
+}
