23ffff065f538958de6ad406d83f9346c3ac1e3e
Jonathan Aguirre <yonathanaguirre@live.com>
2017-03-28 10:54:05 -0300

    funcionando todo
    funcionando todo

--- src/pages/grilla/grilla.ts
+++ src/pages/grilla/grilla.ts
@@ -1,15 +1,32 @@
-
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {Usuario} from "../../pages/grilla/usuario";
 @Component({
   selector: 'page-contact',
   templateUrl: 'grilla.html'
 })
 export class Grilla {
 
-  constructor(public navCtrl: NavController) {
 
+   USUARIOS: Usuario[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+ ];
+
+ Datos(){
+   console.log(this.USUARIOS);
+ }
+  constructor(public navCtrl: NavController) {
+    
   }
+
 
 }
